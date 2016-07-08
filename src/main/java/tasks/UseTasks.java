92f4582fc1f2276ecb65ecb4cd6ca7b569a42cc3
Kenneth Kousen <ken.kousen@kousenit.com>
2016-07-08 12:55:20 -0500

    Add tasks for demo

--- src/main/java/tasks/UseTasks.java
+++ src/main/java/tasks/UseTasks.java
@@ -13,7 +13,14 @@ public class UseTasks {
                 new Task(2, true, 5, "Answer the Bridgekeeper"),
                 new Task(3, false, 6, "Run away from killer rabbit"),
                 new Task(4, true, 4, "Lobbeth thou thy Holy Hand Grenade"),
-                new Task(5, false, 5, "Give a shrubbery to the Knights Who Say Ni!")
+                new Task(5, false, 5, "Give a shrubbery to the Knights Who Say Ni!"),
+                new Task(6, true, 1, "Get taunted by Frenchman"),
+                new Task(7, true, 2, "Weigh a witch against a duck"),
+                new Task(8, false, 4, "Dance and sing and imitate Clark Gable"),
+                new Task(9, true, 3, "Build a giant wooden hare"),
+                new Task(10, false, 2, "Attack Swamp Castle"),
+                new Task(11, true, 1, "Applaud Tim the Enchanter"),
+                new Task(12, false, 2, "Oppress peasant")
         );
 
         double averageDuration = tasks.stream()
