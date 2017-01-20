bace69dd42fc8491f9f439aa38ee5647c0b4027e
Kenneth Kousen <ken.kousen@kousenit.com>
2017-01-20 17:09:28 -0500

    Update grouping demo with additional examples

--- src/main/java/streams/GroupingDemo.java
+++ src/main/java/streams/GroupingDemo.java
@@ -1,32 +1,38 @@
 package streams;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class GroupingDemo {
+    private static <K,V> void printMap(Map<K,V> map) {
+        map.forEach((k,v) -> System.out.println(k + ": " + v));
+    }
+
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("this", "is", "a", "long", "list", "of",
                 "strings", "to", "use", "as", "a", "demo");
 
         Map<Integer, List<String>> lengthMap = strings.stream()
-                .collect(Collectors.groupingBy(String::length));
+                .collect(groupingBy(String::length));
+        printMap(lengthMap);
 
-        lengthMap.forEach((k, v) -> System.out.printf("%d: %s%n", k, v));
+        Map<Integer, Long> counting = strings.stream()
+                .collect(groupingBy(String::length,
+                        counting()));
+        printMap(counting);
 
-        strings.stream()
-                .collect(Collectors.groupingBy(String::length,
-                        Collectors.counting()));
+        Map<Integer, Optional<String>> minBy = strings.stream()
+                .collect(groupingBy(String::length,
+                        Collectors.minBy(comparingInt(String::length))));
+        printMap(minBy);
 
-        strings.stream()
-                .collect(Collectors.groupingBy(String::length,
-                        Collectors.minBy(Comparator.comparingInt(String::length))));
-
-        strings.stream()
-                .collect(Collectors.groupingBy(String::length,
-                        Collectors.maxBy(Comparator.comparingInt(String::length))));
-
+        Map<Integer, Optional<String>> maxBy = strings.stream()
+                .collect(groupingBy(String::length,
+                        Collectors.maxBy(comparingInt(String::length))));
+        printMap(maxBy);
     }
 }
