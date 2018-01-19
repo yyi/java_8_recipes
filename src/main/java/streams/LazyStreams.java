4d1da6786ec5f93b98d4f8309a89afb3e3b3e5d2
Ken Kousen <ken.kousen@kousenit.com>
2018-01-19 17:28:25 -0500

    Reformat code

--- src/main/java/streams/LazyStreams.java
+++ src/main/java/streams/LazyStreams.java
@@ -17,16 +17,16 @@ public class LazyStreams {
     public static void main(String[] args) {
         // Find first even double between 200 and 400 divisible by 3
         int firstEvenDoubleDivBy3 = IntStream.range(100, 200)
-                .map(n -> n * 2)
-                .filter(n -> n % 3 == 0)
-                .findFirst().orElse(0);
+                                             .map(n -> n * 2)
+                                             .filter(n -> n % 3 == 0)
+                                             .findFirst().orElse(0);
         System.out.println(firstEvenDoubleDivBy3);
 
         // Demonstrate laziness using print statements
         firstEvenDoubleDivBy3 = IntStream.range(100, 2_000_000)
-                .map(LazyStreams::multByTwo)
-                .filter(LazyStreams::modByThree)
-                .findFirst().orElse(0);
+                                         .map(LazyStreams::multByTwo)
+                                         .filter(LazyStreams::modByThree)
+                                         .findFirst().orElse(0);
         System.out.printf("First even divisible by 3 is %d%n", firstEvenDoubleDivBy3);
     }
 }
--- src/main/java/streams/StreamsDemo.java
+++ src/main/java/streams/StreamsDemo.java
@@ -11,57 +11,57 @@ import static java.util.stream.Collectors.joining;
 
 public class StreamsDemo {
     private List<String> strings = Arrays.asList("this", "is", "a",
-            "list", "of", "strings");
+                                                 "list", "of", "strings");
 
     public String joinStream() {
         return strings.stream()
-                .collect(Collectors.joining(" "));
+                      .collect(Collectors.joining(" "));
     }
 
     public String joinUpperCase() {
         return strings.stream()
-                .map(String::toUpperCase)
-                .collect(joining(" "));
+                      .map(String::toUpperCase)
+                      .collect(joining(" "));
     }
 
     public int getTotalLength() {
         return strings.stream()
-                .mapToInt(String::length)
-                .sum();
+                      .mapToInt(String::length)
+                      .sum();
     }
 
     public double sumFirstNBigDecimals(int num) {
         return Stream.iterate(BigDecimal.ONE, val -> val.add(BigDecimal.ONE))
-                .limit(num)
-                .mapToDouble(BigDecimal::doubleValue)
-                .sum();
+                     .limit(num)
+                     .mapToDouble(BigDecimal::doubleValue)
+                     .sum();
     }
 
     public BigDecimal sumFirstNBigDecimalsWithPrecision(int num) {
         return Stream.iterate(BigDecimal.ONE, n -> n.add(BigDecimal.ONE))
-                .limit(num)
-                // .peek(x -> System.out.println("The value is " + x))
-                // .reduce(BigDecimal.ZERO, BigDecimal::add);
-                .reduce(BigDecimal.ZERO,
-                        (accumulator, val) -> {
-                            System.out.printf("%s, %s%n", accumulator, val);
-                            return accumulator.add(val);
-                        });
+                     .limit(num)
+                     // .peek(x -> System.out.println("The value is " + x))
+                     // .reduce(BigDecimal.ZERO, BigDecimal::add);
+                     .reduce(BigDecimal.ZERO,
+                             (accumulator, val) -> {
+                                 System.out.printf("%s, %s%n", accumulator, val);
+                                 return accumulator.add(val);
+                             });
     }
 
     public Double sumRandoms1(int num) {
         return Stream.generate(Math::random)
-                .limit(num)
-                .reduce(Double::sum).orElse(0.0);
+                     .limit(num)
+                     .reduce(Double::sum).orElse(0.0);
     }
 
     public Double sumRandoms2(int num) {
         return Stream.generate(Math::random)
-                .limit(num)
-                .reduce((acc, n) -> {
-                    System.out.printf("Acc=%s, n=%s%n", acc, n);
-                    return acc + n;
-                }).orElse(0.0);
+                     .limit(num)
+                     .reduce((acc, n) -> {
+                         System.out.printf("Acc=%s, n=%s%n", acc, n);
+                         return acc + n;
+                     }).orElse(0.0);
     }
 
     public Double sumRandoms3(int num) {
