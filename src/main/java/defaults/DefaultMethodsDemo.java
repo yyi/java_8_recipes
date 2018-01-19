598d9bb4a89135087f1a241b30bd1c23e19402d7
Kenneth Kousen <ken.kousen@kousenit.com>
2018-01-19 17:06:49 -0500

    Update removeIf example to use modifiable list

--- src/main/java/defaults/DefaultMethodsDemo.java
+++ src/main/java/defaults/DefaultMethodsDemo.java
@@ -1,17 +1,20 @@
 package defaults;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DefaultMethodsDemo {
     public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(3, 1, 4, 1, 5, 9);
+        List<Integer> nums = new ArrayList<>();
+        nums.add(-3); nums.add(1); nums.add(4);
+        nums.add(-1); nums.add(5); nums.add(9);
         System.out.println(nums);
 
         // removeIf is a default method in Collection
         // returns true if any elements were removed
         boolean removed = nums.removeIf(n -> n <= 0);
-        System.out.println("Elements were " + (removed ? "" : "NOT") + " removed");
+        System.out.println("Elements were" + (removed ? " " : "NOT ") + "removed");
         System.out.println(nums);
 
         // Iterator has a default forEach method
