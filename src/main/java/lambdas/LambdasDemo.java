d872c3baf00de5ecc15197b72ef8c8f716311fa8
Ken Kousen <ken.kousen@kousenit.com>
2018-08-03 10:30:09 -0500

    Reformat examples

--- src/main/java/lambdas/LambdasDemo.java
+++ src/main/java/lambdas/LambdasDemo.java
@@ -13,7 +13,7 @@ public class LambdasDemo {
     // @SuppressWarnings("Convert2MethodRef")
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("this", "is", "a",
-                "list", "of", "strings");
+                                             "list", "of", "strings");
 
         // Java 7 syntax
         System.out.println("Printing using anonymous inner class:");
@@ -38,34 +38,34 @@ public class LambdasDemo {
         strings.forEach(System.out::println);
 
         int totalLength = strings.stream()
-                .mapToInt(String::length)
-                .sum();
+                                 .mapToInt(String::length)
+                                 .sum();
         System.out.printf("The total length of the strings is %d%n", totalLength);
 
         Stream.of(3, 1, 4, 1, 5, 9)
-                .forEach(System.out::println);
+              .forEach(System.out::println);
 
         // Define Consumer separately
         Consumer<Integer> printer = n -> System.out.println(n);
         Stream.of(3, 1, 4, 1, 5, 9)
-                .forEach(printer);
+              .forEach(printer);
 
         System.out.println();
         // Predicate returns boolean
         Predicate<Integer> mod3 = n -> n % 3 == 0;
         Stream.of(3, 1, 4, 1, 5, 9)
-                .filter(mod3)
-                .forEach(printer);
+              .filter(mod3)
+              .forEach(printer);
 
         System.out.println();
         // Function of one type, returns one type
         IntUnaryOperator doubler = n -> n * 2;
         int sum = IntStream.range(1, 10)
-                .filter(n -> n % 3 == 0)
-                .peek(n -> System.out.println("After the filter: " + n))
-                .map(doubler)
-                .peek(n -> System.out.println("After the doubler: " + n))
-                .sum();
+                           .filter(n -> n % 3 == 0)
+                           .peek(n -> System.out.println("After the filter: " + n))
+                           .map(doubler)
+                           .peek(n -> System.out.println("After the doubler: " + n))
+                           .sum();
         System.out.println("The total is " + sum);
     }
 }
